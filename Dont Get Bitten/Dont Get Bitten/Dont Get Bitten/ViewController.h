3168e8a304a288014ae0197e971b6f3e52bcc58b
Oliver Rodden <olirodd@gmail.com>
2014-09-18 17:19:46 +0100

    dont\ get\ bitten

--- Dont Get Bitten/Dont Get Bitten/Dont Get Bitten/ViewController.h
+++ Dont Get Bitten/Dont Get Bitten/Dont Get Bitten/ViewController.h
@@ -7,10 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <iAd/iAd.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <ADBannerViewDelegate>
+
 @property (weak, nonatomic) IBOutlet UILabel *scoreLbl;
+@property (weak, nonatomic) IBOutlet UILabel *highscoreLbl;
 
 @property (weak, nonatomic) IBOutlet UIImageView *topJaw;
 @property (weak, nonatomic) IBOutlet UIImageView *bottomJaw;
+
+@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *gameOverCol;
+
+@property (weak, nonatomic) IBOutlet UILabel *charCounter;
+
+//@property (weak, nonatomic) IBOutlet ADBannerView *adBanner;
+- (IBAction)playAgainBtnPressed:(id)sender;
+- (IBAction)facebookBtnPressed:(id)sender;
+- (IBAction)twitterBtnPressed:(id)sender;
+
 @end
