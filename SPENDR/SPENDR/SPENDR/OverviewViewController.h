1e9aed81821d3981cd6a658d4e7054b3997fad38
Oliver Rodden <olirodd@gmail.com>
2015-01-12 09:38:43 +0000

    SPENDR added

---
+++ SPENDR/SPENDR/SPENDR/OverviewViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SPENDR
+//
+//  Created by Oliver Rodden on 12/01/2015.
+//  Copyright (c) 2015 Oliver Rodden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OverviewViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *entrySegControl;
+
+@end
+
---
+++ SPENDR/SPENDR/SPENDR/OverviewViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SPENDR
+//
+//  Created by Oliver Rodden on 12/01/2015.
+//  Copyright (c) 2015 Oliver Rodden. All rights reserved.
+//
+
+#import "OverviewViewController.h"
+
+@interface OverviewViewController ()
+
+@end
+
+@implementation OverviewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.entrySegControl.selectedSegmentIndex = -1;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
