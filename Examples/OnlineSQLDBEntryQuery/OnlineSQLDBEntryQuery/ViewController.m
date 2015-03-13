d06ea440a44af9f62ba0eb006a3652bdd98121b5
Oliver Rodden <olirodd@gmail.com>
2015-03-13 13:40:02 +0000

    more bs

--- Examples/OnlineSQLDBEntryQuery/OnlineSQLDBEntryQuery/ViewController.m
+++ Examples/OnlineSQLDBEntryQuery/OnlineSQLDBEntryQuery/ViewController.m
@@ -19,26 +19,26 @@
     // Do any additional setup after loading the view, typically from a nib.
 //    NSURLRequest *requestObj = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://192.168.21.210/github/web/grammanager/select2.php"]];
 
-    NSURLRequest *requestObj = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://xpand.editionthree.com"]];
-    [self.webView loadRequest:requestObj];
+//    NSURLRequest *requestObj = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://xpand.editionthree.com"]];
+//    [self.webView loadRequest:requestObj];
     
     
     
-//    NSString *urlForPostData = [NSString stringWithFormat:@"http://192.168.21.210/github/web/grammanager/select2.php"];
+    NSString *urlForPostData = [NSString stringWithFormat:@"http://192.168.21.210/github/web/grammanager/select2.php"];
 
 //    NSString *urlForPostData = [NSString stringWithFormat:@"http://xpand.editionthree.com"];
 //    
-//    [NSURLConnection sendAsynchronousRequest:[[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlForPostData]] queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
-//        if (error) {
-//            NSLog(@"Error 1 %@", error);
-//        } else {
-//            NSLog(@"%@", data);
-//            
-//            NSDictionary *jsonDictionary=[NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-//            
-//           NSLog(@"#### %@", jsonDictionary);
-//        }
-//    }];
+    [NSURLConnection sendAsynchronousRequest:[[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlForPostData]] queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+        if (error) {
+            NSLog(@"Error 1 %@", error);
+        } else {
+            NSLog(@"%@", data);
+            
+            NSDictionary *jsonDictionary=[NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            
+           NSLog(@"#### %@", jsonDictionary);
+        }
+    }];
 }
 
 -(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
